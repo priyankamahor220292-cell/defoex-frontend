c75031b7c42035c992dfed39775620c4fc72713d
priya-ji <priyankamahor32@gmail.com>
2026-06-25 23:24:40 +0530

    put api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001',
+  baseURL: process.env.REACT_APP_API_URL || 'http://3.110.209.154/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
